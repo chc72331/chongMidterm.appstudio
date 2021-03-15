a7b9d6e744ffb9f23d651cd4ed2675be28217751
chc72331 <chc72331@creighton.edu>
2021-03-15 15:23:24 -0500

    Update animals.js

--- forms/animals/animals.js
+++ forms/animals/animals.js
@@ -1,8 +1,10 @@
-myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+/*
+let myAnimals = ['dog', 'cat', 'horse', 'meerkat']
 
-userAnimal = prompt('Add an animal!')
-smallAnimal = userAnimal.toLowerCase()
+let userAnimal = prompt('Add an animal!')
+let smallAnimal = userAnimal.toLowerCase()
 
 myAnimals.push(smallAnimal)
 
 console.log(`The last animal is a/an ${myAnimals[myAnimals.length - 1 ]}.`)
+*/
