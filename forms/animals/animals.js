b48d1eb7685de92823358f3702569bfa16d0eb8b
chc72331 <chc72331@creighton.edu>
2021-03-15 14:35:48 -0500

    Update animals.js

--- forms/animals/animals.js
+++ forms/animals/animals.js
@@ -1,1 +1,8 @@
-// Github test
+myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+
+userAnimal = prompt('Add an animal!')
+smallAnimal = userAnimal.toLowerCase()
+
+myAnimals.push(smallAnimal)
+
+console.log(`The last animal is a/an ${myAnimals[myAnimals.length - 1 ]}.`)
