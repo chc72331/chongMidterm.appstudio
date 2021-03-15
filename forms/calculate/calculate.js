5acecca2e3a638b956db31d9479c03e1526ad342
chc72331 <chc72331@creighton.edu>
2021-03-15 15:22:30 -0500

    Create calculate.js

---
+++ forms/calculate/calculate.js
@@ -0,0 +1,15 @@
+
+/*
+function calcAvgSqaure (a, b, c) {
+  let answer = ((a + b + c)/3) * Math.pow(a, 2)
+  return answer
+}
+
+userNum1 = Number(prompt('Enter the first number'))
+userNum2 = Number(prompt('Enter the second number'))
+userNum3 = Number(prompt('Enter the third number'))
+
+let result = calcAvgSqaure(userNum1, userNum2, userNum3)
+
+console.log(`The answer is ${result}.`)
+*/
